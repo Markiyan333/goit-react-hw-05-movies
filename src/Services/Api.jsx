6ca75119e4755941efd1b9b49d575a988a69b226
Markiyan333 <markiyan333@gmail.com>
2023-04-15 23:35:05 +0300

    well done

---
+++ src/Services/Api.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = {
+  api_key: '1efdf9e7a9ff7e2165fc0b365172fcf9',
+};
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+const URLSearchParams = {
+  trending: '/trending/movie/week',
+  search: '/search/movie',
+  movieDetails: '/movie/',
+};
+
+export const getTrendingMovies = async () => {
+  const response = await axios.get(URLSearchParams.trending);
+  return response.data.results;
+};
+
+export const getSearchingMovies = async searchFilm => {
+  const options = {
+    params: { query: searchFilm },
+  };
+  const response = await axios.get(URLSearchParams.search, options);
+  return response.data.results;
+};
+
+export const getMovieDetails = async id => {
+  const response = await axios.get(URLSearchParams.movieDetails + id);
+  console.log(response.data);
+  return response.data;
+};
+
+export const getMovieReviews = async id => {
+  const response = await axios.get(
+    URLSearchParams.movieDetails + id + '/reviews'
+  );
+  console.log(response.data.results);
+  return response.data.results;
+};
+
+export const getMovieCast = async id => {
+  const response = await axios.get(
+    URLSearchParams.movieDetails + id + '/credits'
+  );
+  console.log(response.data.cast);
+  return response.data.cast;
+};
---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,25 @@
+import { Suspense } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import css from './Layout.module.css';
+
+export const Layout = () => {
+  return (
+    <div>
+      <ul className={css.LayoutUl}>
+        <li className={css.LayoutIl}>
+          <NavLink className={css.LayoutIl} to="/">
+            <a className={css.Text}>Home</a>
+          </NavLink>
+        </li>
+        <li className={css.LayoutIl}>
+          <NavLink className={css.LayoutIl} to="/movies">
+            <a className={css.Text}>Movies</a> 
+          </NavLink>
+        </li>
+      </ul>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
